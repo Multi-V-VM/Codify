5015097c007c0ac3269829e96d99169ca15a5074
Yiwei Yang <40686366+vickiegpt@users.noreply.github.com>
2026-03-18 00:35:33 -0700

    Replace wget with curl for downloading node.wasm

--- downloadFrameworks.sh
+++ downloadFrameworks.sh
@@ -52,7 +52,8 @@ done
 # Node.js
 mkdir -p NodeJS
 cd NodeJS
-wget https://asplos.dev/about/node.wasm
+curl -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36" \
+  -L -o node.wasm https://asplos.dev/about/node.wasm
 curl -OL https://github.com/1Conan/nodejs-mobile/releases/download/v18.19.0-ios/NodeMobile.xcframework.zip
 unzip -q NodeMobile.xcframework.zip
 rm -f NodeMobile.xcframework.zip
